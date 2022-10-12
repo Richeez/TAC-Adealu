329009f37933c94f4d4bb6fa3e9fc801c423d89b
DevRichard <saturdayrichard98@gmail.com>
2022-10-12 13:37:00 +0100

    Pause Slide Interval on mouse hover

--- src/Components/carousel/ImageSlides.jsx
+++ src/Components/carousel/ImageSlides.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import { BGImage } from '../../Styles/Styled';
 import CarouselControls from './CarouselControls';
 import CarouselIndicator from './CarouselIndicator';
@@ -7,32 +7,54 @@ import CarouselIndicator from './CarouselIndicator';
 const ImageSlides = ({slides}) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
-//   useEffect(() => {
-// const slideInterval = setInterval(() => {
-//   setCurrentSlide(prev => prev < slides.length - 1 ? prev + 1 : 0 )  
-// }, 3000)  
-//     return () => clearInterval(slideInterval)   
-//   }, [slides.length]);
+  const slideInterval = useRef(null)
+
+  const startSlideTimer = ()=>{
+    stopSlideTimer()
+
+    slideInterval.current = setInterval(() => {
+      setCurrentSlide(prev => prev < slides.length - 1 ? prev + 1 : 0 )  
+    }, 3000)  
+    
+  }
+
+  const stopSlideTimer = () =>{
+    if (slideInterval.current) {
+      
+      clearInterval(slideInterval.current)
+    }
+  }
 
   const prev = ()=>{
+    startSlideTimer()
+
     const index = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
 
     setCurrentSlide(index)
   }
 
   const next = ()=>{
+    startSlideTimer()
+
     const index = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
 
     setCurrentSlide(index)
   }
+
+  useEffect(() => {
+    startSlideTimer()
+
+    return () =>  stopSlideTimer()  
+  }, []);
+
   
 
   return (
     <>
-<div className="inner-container">
+<div onMouseEnter={stopSlideTimer} onMouseOut={startSlideTimer} className="inner-container">
     {slides.map((slide, index) => (
    <div className="img" style={{transform:`translateX(${currentSlide * -100}%)`}}>
-    <BGImage src={slide.img} key={index} alt='image'/>
+    <BGImage src={slide.img}  key={index} alt='image'/>
               
             </div>
 
